5e31d669b419c7dce504c5f256a7997701e6de15
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-18 15:34:09 -0500

    Fetch to Decks controller on mount of CardContainer

--- src/Containers/CardContainer.js
+++ src/Containers/CardContainer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BASE_URL } from '../App'
+import { connect } from 'react-redux'
 
 class CardContainer extends React.Component{
 
   componentDidMount(){
-    console.log("CardContainer Mounted")
+    let data = {
+      language_id: this.props.selectedLanguageId,
+      deck_id: this.props.selectedDeckId
+    }
+    
+    fetch( `${BASE_URL}/get_deck_words`, {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }, 
+      body: JSON.stringify( data )
+    } )
   }
   render(){
     return (
@@ -14,4 +28,8 @@ class CardContainer extends React.Component{
   }
 }
 
-export default CardContainer
+const mapStateToProps = state => ({
+  selectedLanguageId: state.selectedLanguage.id,
+  selectedDeckId: state.selectedDeck.id
+})
+export default connect(mapStateToProps) (CardContainer)
