3ab06cbfd6788bbb4ecee81b37f785d4298b6916
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-18 16:52:30 -0500

    Start study card component

--- src/Components/StudyCard.js
+++ src/Components/StudyCard.js
@@ -2,20 +2,27 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class StudyCard extends React.Component {
+
   render(){
+    const { deckWords } = this.props
+
+    let currentCard = 0
+
+
+
     return(
       <div>
-        {
-          this.props.deckWords.map( word => { 
-            return (
-              <div className="ui card">
-                <h1>{word.word_target_language}</h1>
-                { word.pronunciation ? (<p><em>{word.pronunciation}</em></p>) : null }
-                <p>{word.word_english}</p>
-              </div>
-            )
-          } )
-        }
+
+        <div className="ui card">
+
+        </div>
+
+        <div>
+          <button> - </button>
+            {currentCard+1} of {deckWords.length}
+          <button> + </button>
+        </div>
+
       </div>
     )
   }
@@ -26,3 +33,18 @@ const mapStateToProps = state => ({
 })
 
 export default connect(mapStateToProps) (StudyCard)
+
+
+ // <div>
+      //   {
+      //     this.props.deckWords.map( word => { 
+      //       return (
+      //         <div className="ui card">
+      //           <h1>{word.word_target_language}</h1>
+      //           { word.pronunciation ? (<p><em>{word.pronunciation}</em></p>) : null }
+      //           <p>{word.word_english}</p>
+      //         </div>
+      //       )
+      //     } )
+      //   }
+      // </div>
