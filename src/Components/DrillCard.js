c47a7b694c63867ede0b45f7d8e7a791e5101431
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-23 15:24:12 -0500

    Build DrillCard - render first cardObject, set answer in state on mount of component

--- src/Components/DrillCard.js
+++ src/Components/DrillCard.js
@@ -1,19 +1,46 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class DrillCard extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      currentCard: 0, 
+      answer: ""
+    }
+  }
+
+  componentDidMount(){
+    const { currentCard } = this.state
+
+    this.setState( {
+      answer: this.props.deckWords[currentCard].word_english
+    } )
+  }
+
 
   render(){
+    const { deckWords } = this.props
+    console.log( deckWords )
     return(
-      <div className="drill-card-container">
-        <div className="drill-card">
-          <div className="drill-card-front">
-            Drill Card Goes Here
+    <div>
+        <div className="drill-card-container">
+          <div className="drill-card">
+            <div className="drill-card-front">
+              <h1>{deckWords[this.state.currentCard].word_target_language}</h1>
+            </div>
           </div>
         </div>
-      </div>
+  
+        <button onClick={} >Next Card</button>
+    </div>
     )
   }
 
 }
 
-export default DrillCard
+const mapStateToProps = state => ({
+  deckWords: state.deckWords
+})
+
+export default  connect(mapStateToProps ) (DrillCard)
