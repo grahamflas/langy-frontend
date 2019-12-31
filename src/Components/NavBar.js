fe0ac1b263410755004613f595739f92aa1b5a61
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-31 18:25:07 -0500

    Greet user in NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const NavBar = () => {
+const NavBar = (props) => {
+  let { currentUser: {username} } = props
   return (
     <div className="navbar ui inverted teal menu">
       <NavLink className="ui button" to="/languages">Languages</NavLink>
+  <div>{`Welcome, ${username}`}</div>
     </div>
   )
 }
 
-export default NavBar
+const mapStateToProps = state => ( {
+  currentUser: state.currentUser
+} )
+export default connect(mapStateToProps) (NavBar)
