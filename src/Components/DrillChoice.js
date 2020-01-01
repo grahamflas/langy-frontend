5221cdb751bdb09117b4760db486e98d92928e45
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-01 13:18:19 -0500

    Style drill choices

--- src/Components/DrillChoice.js
+++ src/Components/DrillChoice.js
@@ -33,7 +33,7 @@ class DrillChoice extends React.Component {
   render() {
     let { word } = this.props
     return (
-      <div className="ui card drill-choice" onClick={this.handleClick}>
+      <div className="drill-choice" onClick={this.handleClick}>
         {word}
       </div>
     )
